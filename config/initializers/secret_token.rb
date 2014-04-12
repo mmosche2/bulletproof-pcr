90f6e155db35f715b49e8a536e30388984da44bb
Mark Moschel <mark.moschel@gmail.com>
2014-04-11 21:57:24 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BulletproofPcr::Application.config.secret_key_base = '0fc2c76566e20b8fe4d06d561fb1a82accb51a2474301327e49d5aa878a4dee139a96f8c1265404463122adc92c34a7e5b27355068e4a65b6fb1c8b91836d277'
